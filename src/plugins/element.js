e7948d1642af721b411bba2c7432f7ae4e281ba9
mageeliu <7640620+mageeliu@user.noreply.gitee.com>
2020-09-10 20:08:00 +0800

    never give up

---
+++ src/plugins/element.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+// 导入弹框提示组件
+Vue.prototype.$message = Message
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
