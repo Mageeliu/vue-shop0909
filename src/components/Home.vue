42d34371b05dda7dfb91e717f7a9b570dd16756a
mageeliu <7640620+mageeliu@user.noreply.gitee.com>
2020-09-10 23:25:51 +0800

    home页面

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,121 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <!-- 头部区域 -->
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="@/assets/heima.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 主体部分 -->
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <div class="toggle-button">|||</div>
+        <!-- 侧边栏菜单 -->
+        <el-menu background-color="#333744"
+        text-color="#fff"
+        active-text-color="#409EFF"
+        unique-opened>
+        <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <!-- 一级菜单的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span> {{item.authName}} </span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+              <!-- 图标 -->
+              <i class="el-icon-menu"></i>
+              <!-- 文本 -->
+              <span>{{subItem.authName}}</span>
+            </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 内容区 -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menuList: [],
+      iconsObj: {
+        125: 'iconfont icon-user',
+        103: 'iconfont icon-tijikongjian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-danju',
+        145: 'iconfont icon-baobiao'
+
+      }
+    }
+  },
+  created() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有的菜单
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.data.msg)
+      this.menuList = res.data
+      console.log(res)
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home-container{
+  height: 100%;
+}
+.el-header{
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 10px;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div{
+    display: flex;
+    align-items: center;
+    span{
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside{
+  background-color: #333744;
+  color: #fff;
+  .el-menu{
+    border-right: none;
+  }
+  .toggle-button{
+    background-color: #4a5064;
+    font-size: 10px;
+    line-height: 50px;
+
+  }
+}
+.el-main{
+  background-color: #eaedf1;
+}
+.iconfont{
+  margin-right: 10px;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,19 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItemGroup,
+  MenuItem
+} from 'element-ui'
 
 // 导入弹框提示组件
 Vue.prototype.$message = Message
@@ -8,3 +22,11 @@ Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
